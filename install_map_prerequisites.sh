742201af86b0ffcd4a94ee7b0f8fd20241bccddb
Edward Delaporte <edward@delaporte.us>
2025-04-08 19:34:40 -0500

    Simpler map tool

--- install_map_prerequisites.sh
+++ install_map_prerequisites.sh
@@ -1,1 +1,7 @@
-sudo apt install -y cmake libgd-dev libhiredis-dev libleveldb-dev libpq-dev libsqlite3-dev zlib1g-dev libzstd-dev
+sudo apt install cmake libgd-dev libhiredis-dev libleveldb-dev libpq-dev libsqlite3-dev zlib1g-dev libzstd-dev
+mkdir -p ~/bin/luanti
+cd ~/bin/luanti
+git clone https://github.com/luanti-org/minetestmapper
+cd minetestmapper
+cmake . -DENABLE_LEVELDB=1
+make -j$(nproc)
